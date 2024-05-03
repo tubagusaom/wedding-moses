2b7cbf54a0097ec8d82993245b8e17bb374b5813
Tera Byte <tubagus.aom.swk@gmail.com>
2024-05-04 02:40:59 +0700

    mergephoto

--- application/views/templates/bootstraps/gallery.php
+++ application/views/templates/bootstraps/gallery.php
@@ -17,158 +17,84 @@
 							<p>Check the examples file <code>examples.gallery.js</code> for more information.</p>
 
 							<div class="row mt-xlg">
-								<div class="col-md-4 col-md-offset-1">
-									<div class="thumb-gallery">
-										<div class="owl-carousel owl-theme manual thumb-gallery-detail show-nav-hover" id="thumbGalleryDetail">
-											<div>
-												<span class="img-thumbnail">
-													<img alt="Project Image" src="img/projects/project.jpg" class="img-responsive">
-												</span>
-											</div>
-											<div>
-												<span class="img-thumbnail">
-													<img alt="Project Image" src="img/projects/project-2.jpg" class="img-responsive">
-												</span>
-											</div>
-											<div>
-												<span class="img-thumbnail">
-													<img alt="Project Image" src="img/projects/project-4.jpg" class="img-responsive">
-												</span>
-											</div>
-											<div>
-												<span class="img-thumbnail">
-													<img alt="Project Image" src="img/projects/project-5.jpg" class="img-responsive">
-												</span>
-											</div>
-											<div>
-												<span class="img-thumbnail">
-													<img alt="Project Image" src="img/projects/project-6.jpg" class="img-responsive">
-												</span>
-											</div>
-											<div>
-												<span class="img-thumbnail">
-													<img alt="Project Image" src="img/projects/project-2.jpg" class="img-responsive">
-												</span>
-											</div>
-											<div>
-												<span class="img-thumbnail">
-													<img alt="Project Image" src="img/projects/project-4.jpg" class="img-responsive">
-												</span>
-											</div>
-										</div>
-										<div class="owl-carousel owl-theme manual thumb-gallery-thumbs mt" id="thumbGalleryThumbs">
-											<div>
-												<span class="img-thumbnail cur-pointer">
-													<img alt="Project Image" src="img/projects/project.jpg" class="img-responsive">
-												</span>
-											</div>
-											<div>
-												<span class="img-thumbnail cur-pointer">
-													<img alt="Project Image" src="img/projects/project-2.jpg" class="img-responsive">
-												</span>
-											</div>
-											<div>
-												<span class="img-thumbnail cur-pointer">
-													<img alt="Project Image" src="img/projects/project-4.jpg" class="img-responsive">
-												</span>
-											</div>
-											<div>
-												<span class="img-thumbnail cur-pointer">
-													<img alt="Project Image" src="img/projects/project-5.jpg" class="img-responsive">
-												</span>
-											</div>
-											<div>
-												<span class="img-thumbnail cur-pointer">
-													<img alt="Project Image" src="img/projects/project-6.jpg" class="img-responsive">
-												</span>
-											</div>
-											<div>
-												<span class="img-thumbnail cur-pointer">
-													<img alt="Project Image" src="img/projects/project-2.jpg" class="img-responsive">
-												</span>
-											</div>
-											<div>
-												<span class="img-thumbnail cur-pointer">
-													<img alt="Project Image" src="img/projects/project-4.jpg" class="img-responsive">
-												</span>
-											</div>
-										</div>
-									</div>
-								</div>
+								
                 
-								<div class="col-md-4 col-md-offset-2">
+								<div class="col-md-12">
 									<div class="thumb-gallery">
 										<div class="owl-carousel owl-theme manual thumb-gallery-detail" id="thumbGalleryDetail2">
 											<div>
 												<span class="img-thumbnail">
-													<img alt="Project Image" src="img/projects/project.jpg" class="img-responsive">
+													<img alt="Project Image" src="<?=base_url()?>_assets/img/prewed-moses/2.jpg" class="img-responsive">
 												</span>
 											</div>
 											<div>
 												<span class="img-thumbnail">
-													<img alt="Project Image" src="img/projects/project-2.jpg" class="img-responsive">
+													<img alt="Project Image" src="<?=base_url()?>_assets/img/prewed-moses/3.jpg" class="img-responsive">
 												</span>
 											</div>
 											<div>
 												<span class="img-thumbnail">
-													<img alt="Project Image" src="img/projects/project-4.jpg" class="img-responsive">
+													<img alt="Project Image" src="<?=base_url()?>_assets/img/prewed-moses/4.jpg" class="img-responsive">
 												</span>
 											</div>
 											<div>
 												<span class="img-thumbnail">
-													<img alt="Project Image" src="img/projects/project-5.jpg" class="img-responsive">
+													<img alt="Project Image" src="<?=base_url()?>_assets/img/prewed-moses/5.jpg" class="img-responsive">
 												</span>
 											</div>
 											<div>
 												<span class="img-thumbnail">
-													<img alt="Project Image" src="img/projects/project-6.jpg" class="img-responsive">
+													<img alt="Project Image" src="<?=base_url()?>_assets/img/prewed-moses/6.jpg" class="img-responsive">
 												</span>
 											</div>
 											<div>
 												<span class="img-thumbnail">
-													<img alt="Project Image" src="img/projects/project-2.jpg" class="img-responsive">
+													<img alt="Project Image" src="<?=base_url()?>_assets/img/prewed-moses/7.jpg" class="img-responsive">
 												</span>
 											</div>
 											<div>
 												<span class="img-thumbnail">
-													<img alt="Project Image" src="img/projects/project-4.jpg" class="img-responsive">
+													<img alt="Project Image" src="<?=base_url()?>_assets/img/prewed-moses/8.jpg" class="img-responsive">
 												</span>
 											</div>
 										</div>
+
+										<!-- batas -->
+
 										<div class="owl-carousel owl-theme manual thumb-gallery-thumbs mt" id="thumbGalleryThumbs2">
 											<div>
 												<span class="img-thumbnail cur-pointer">
-													<img alt="Project Image" src="img/projects/project.jpg" class="img-responsive">
+													<img alt="Project Image" src="<?=base_url()?>_assets/img/prewed-moses/2.jpg" class="img-responsive">
 												</span>
 											</div>
 											<div>
 												<span class="img-thumbnail cur-pointer">
-													<img alt="Project Image" src="img/projects/project-2.jpg" class="img-responsive">
+													<img alt="Project Image" src="<?=base_url()?>_assets/img/prewed-moses/3.jpg" class="img-responsive">
 												</span>
 											</div>
 											<div>
 												<span class="img-thumbnail cur-pointer">
-													<img alt="Project Image" src="img/projects/project-4.jpg" class="img-responsive">
+													<img alt="Project Image" src="<?=base_url()?>_assets/img/prewed-moses/4.jpg" class="img-responsive">
 												</span>
 											</div>
 											<div>
 												<span class="img-thumbnail cur-pointer">
-													<img alt="Project Image" src="img/projects/project-5.jpg" class="img-responsive">
+													<img alt="Project Image" src="<?=base_url()?>_assets/img/prewed-moses/5.jpg" class="img-responsive">
 												</span>
 											</div>
 											<div>
 												<span class="img-thumbnail cur-pointer">
-													<img alt="Project Image" src="img/projects/project-6.jpg" class="img-responsive">
+													<img alt="Project Image" src="<?=base_url()?>_assets/img/prewed-moses/6.jpg" class="img-responsive">
 												</span>
 											</div>
 											<div>
 												<span class="img-thumbnail cur-pointer">
-													<img alt="Project Image" src="img/projects/project-2.jpg" class="img-responsive">
+													<img alt="Project Image" src="<?=base_url()?>_assets/img/prewed-moses/7.jpg" class="img-responsive">
 												</span>
 											</div>
 											<div>
 												<span class="img-thumbnail cur-pointer">
-													<img alt="Project Image" src="img/projects/project-4.jpg" class="img-responsive">
+													<img alt="Project Image" src="<?=base_url()?>_assets/img/prewed-moses/8.jpg" class="img-responsive">
 												</span>
 											</div>
 										</div>
