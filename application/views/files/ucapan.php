3b2b4a1508821d005a69f19251c2fb16b3dfb63b
tubagusaom <tubagus.aom.swk@gmail.com>
2024-05-09 11:52:07 +0700

    mergeserverupdate

--- application/views/files/ucapan.php
+++ application/views/files/ucapan.php
@@ -1,4 +1,4 @@
-[
+[{"id":"4","datetime":"09 May 2024 - 11:51:12 am","name":"ddd","message":"ddd","confirm":"0","browser":"Firefox","browser_version":"125.0","os":"Unknown Windows OS","ip_address":"124.158.184.180"},
   {"id":"2","datetime":"08 June 2024 - 02:05:30 am","name":"Bjorka","message":"\u0423\u0440\u0430","confirm":"1","browser":"XXX","browser_version":"666.6.6.6","os":"Linux","ip_address":"::x"},
   {"id":"1","datetime":"08 June 2024 - 00:00:00 pm","name":"TeraBytee","message":"Congratulation","confirm":"1","browser":"XXX","browser_version":"666.6.6.6","os":"Linux","ip_address":"::x"},
   {"id":"1","datetime":"17 August 2013 - 13:13:13 am","name":"Anonimouse","message":"Congratulation","confirm":"0","browser":"XXX","browser_version":"666.6.6.6","os":"Linux","ip_address":"::x"}
