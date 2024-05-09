30b44c3100c9cd76ab4873f7af968e5640d4edf1
Tera Byte <tubagus.aom.swk@gmail.com>
2024-05-09 11:29:15 +0700

    mergeupdate

--- application/views/files/ucapan.php
+++ application/views/files/ucapan.php
@@ -1,5 +1,5 @@
 [
   {"id":"2","datetime":"08 June 2024 - 02:05:30 am","name":"Bjorka","message":"\u0423\u0440\u0430","confirm":"1","browser":"XXX","browser_version":"666.6.6.6","os":"Linux","ip_address":"::x"},
-  {"id":"1","datetime":"17 August 2013 - 00:00:00 am","name":"TeraBytee","message":"Congratulation","confirm":"1","browser":"XXX","browser_version":"666.6.6.6","os":"Linux","ip_address":"::x"},
+  {"id":"1","datetime":"08 June 2024 - 00:00:00 pm","name":"TeraBytee","message":"Congratulation","confirm":"1","browser":"XXX","browser_version":"666.6.6.6","os":"Linux","ip_address":"::x"},
   {"id":"1","datetime":"17 August 2013 - 13:13:13 am","name":"Anonimouse","message":"Congratulation","confirm":"0","browser":"XXX","browser_version":"666.6.6.6","os":"Linux","ip_address":"::x"}
 ]
