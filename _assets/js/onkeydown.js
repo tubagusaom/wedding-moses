73b8448f1187ee0512c93a17363fd9cdaa1050a6
Tera Byte <tubagus.aom.swk@gmail.com>
2024-05-22 10:26:08 +0700

    mergeonkey

--- _assets/js/onkeydown.js
+++ _assets/js/onkeydown.js
@@ -8,7 +8,7 @@ document.querySelectorAll('.disabled').forEach(element => {
 
 document.onkeydown = function(e) {
         if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 || e.keyCode === 117)) {//Alt+c, Alt+v will also be disabled sadly.
-            alert('hayooooo mau ngapain :p');
+            alert('hayooooo mau ngapain !!!');
         }
         return false;
 };
