64c1ce443b49e85d1a7aa2403d9235810fb92d1f
Tera Byte <tubagus.aom.swk@gmail.com>
2024-05-22 14:48:34 +0700

    mergeonkey

--- application/views/files/ucapan.php
+++ application/views/files/ucapan.php
@@ -1,5 +1,5 @@
 [
-  {"id":"2","datetime":"08 June 2024 - 02:05:30 am","name":"Bjorka","message":"Счастливый 🍻","confirm":"1","browser":"XXX","browser_version":"666.6.6.6","os":"Linux","ip_address":"::x"},
+  {"id":"2","datetime":"08 June 2024 - 02:05:30 am","name":"Bjorka","message":"счастливая свадьба 🍻","confirm":"1","browser":"XXX","browser_version":"666.6.6.6","os":"Linux","ip_address":"::x"},
   {"id":"1","datetime":"08 June 2024 - 00:00:00 pm","name":"TeraBytee","message":"Happy Wedding 🍻","confirm":"1","browser":"XXX","browser_version":"666.6.6.6","os":"Linux","ip_address":"::x"},
   {"id":"1","datetime":"17 August 2013 - 13:13:13 am","name":"Anonimouse","message":"Congratulation 🍻","confirm":"0","browser":"XXX","browser_version":"666.6.6.6","os":"Linux","ip_address":"::x"}
 ]
