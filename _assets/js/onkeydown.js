44d2fb5dc944c3bb35ae2ca513ca6e40cf61a5ac
Tera Byte <tubagus.aom.swk@gmail.com>
2024-05-30 10:20:03 +0700

    mergeonclick

--- _assets/js/onkeydown.js
+++ _assets/js/onkeydown.js
@@ -1,14 +1,21 @@
 document.addEventListener('contextmenu', event => {
     event.preventDefault();
+    alert('hayooooo mau ngapain !!!');
 });
+// document.addEventListener("contextmenu", function(e){
+//     e.preventDefault();
+// }, false);
 
-document.querySelectorAll('.disabled').forEach(element => {
-    element.style.pointerEvents = 'none';
-});
 
-document.onkeydown = function(e) {
-        if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 || e.keyCode === 117)) {//Alt+c, Alt+v will also be disabled sadly.
-            alert('hayooooo mau ngapain !!!');
-        }
-        return false;
-};
+
+// document.querySelectorAll('.disabled').forEach(element => {
+//     element.style.pointerEvents = 'none';
+// });
+
+// document.onkeydown = function(e) {
+//         if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 || e.keyCode === 117)) {//Alt+c, Alt+v will also be disabled sadly.
+//             alert('hayooooo mau ngapain !!!');
+//         }
+//         return false;
+// };
+
